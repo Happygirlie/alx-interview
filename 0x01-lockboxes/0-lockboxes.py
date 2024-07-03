e0d623d657ae65c32c9716a50a3a8f5b217133fa
Happygirlie <happinesschinweopara@gmail.com>
2024-07-03 14:30:36 +0000

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-'''LockBoxes Challenge'''
+"""BOXES BOXES"""
 
 
 def canUnlockAll(boxes):
-    '''determines if all the boxes can be opened or not
-    Returns:
-        True: all boxes can be opened
-        False: not all boxes can be opened
-    '''
-    length = len(boxes)
-    keys = set()
-    opened_boxes = []
-    i = 0
+    """
+    take boxes
+        create set of keys
+            go to box0
+                get all keys and add them setofkeys
+            start opening boxes from setofkeys
+                go to each box of each key
+                    and take the keys from it and add them to set of keys
+                keep loping through all setof keys
+            ignore keys that dont have box
+            track opening of boxes by a counter, if at end it
+            equal to lentgh of boxes it mean all boxes unlock
+            OPTIMIZE IDEA :
+                if we add 0 to setofkeys at start, we dont need for in 23
+    """
+    total_boxes = len(boxes)
+    setofkeys = [0]
+    counter = 0
+    index = 0
 
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
-        for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
-            break
- 
-    for i in range(length):
-                                                                                                                                if i not in opened_boxes and i != 0:
-                                                                                                                                    return False
-                                                                                                                            return True
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
+        for key in boxes[setkey]:
+            if 0 < key < total_boxes and key not in setofkeys:
+                setofkeys.append(key)
+                counter += 1
+        index += 1
+
+    return counter == total_boxes - 1
